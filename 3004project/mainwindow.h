828ce9e033bf6876ec526c51d4fa20b55b86813a
Justin Tran <justin.train101@gmail.com>
2021-04-09 20:14:05 -0400

    Update mainwindow.h

--- 3004project/mainwindow.h
+++ 3004project/mainwindow.h
@@ -47,7 +47,6 @@ private:
     QStringListModel* model; // check
     QTimer *timer; // check
     Ui::MainWindow *ui;
-    int index;
 
     void navigateBackScreens(View* destination);
     void navigateMenu(int index);
